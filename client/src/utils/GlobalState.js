28489c959a10bfa152eb4d1bc3ddc10d7d66b4ec
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-14 12:03:42 -0700

    Adding actions

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,11 +1,36 @@
 import React, {createContext, useReducer, useContext} from "react"
+import {UPDATE_BOOKS, SET_CURRENT_BOOK} from "./action"
 
-const BookContext = createContext({})
+const BookContext = createContext({
+    currentBook: {},
+    books: []
+})
 const {Provider} = BookContext
 
 const reducer = (state, action) => {
-
+    switch (action.type) {
+        case UPDATE_BOOKS:
+            return {
+                ...state,
+                books: action.books
+            }
+            break
+        case SET_CURRENT_BOOK:
+            return {
+                ...state,
+                currentBook: action.book
+            }
+            break
+        default:
+            return state
+    }
 }
+
+//When I want to set new book to current book:
+// -----> dispatch({action: SET_CURRENT_BOOK}, book: <new Current Book object>})
+
+// When I want to refresh my database of books:
+// -----> dispatch({action: UPDATE_BOOKS, books: <array of books from API call>})
 
 const BookProvider = () => {
 
